aa50bf8ea14e920146d658bf5e0f5489d2d662fc
pandaXcore <pandadesign7@gmail.com>
2025-10-18 14:59:44 +0500

    moved module to folder as packet

--- game.py
+++ game.py
@@ -1,8 +1,14 @@
-from parts import Board
+from gameparts.parts import Board
 
-game = Board()
-game.display()
-game.make_move(1, 1, 'X')
-game.make_move(0, 0, 'O')
-print('Ход сделан!')
-game.display()
+
+def main():
+    game = Board()
+    game.display()
+    game.make_move(1, 1, 'X')
+    game.make_move(0, 0, 'O')
+    print('Ход сделан!')
+    game.display()
+
+
+if __name__ == '__main__':
+    main()
